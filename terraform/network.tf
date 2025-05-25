b957ca3c389c4de8476803ba1301021905eb853d
Valda <valda.bimbirule1@gmail.com>
2025-05-25 11:51:39 +0300

    Using module for network

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,55 +1,12 @@
-resource "aws_vpc" "my_vpc" {
-    cidr_block           = "10.10.0.0/16"
-    enable_dns_support   = true
-    enable_dns_hostnames = true
-}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name   = "my-vpc"
+  cidr   = "10.0.0.0/16"
 
-resource "aws_subnet" "public_a" {
-  vpc_id                  = aws_vpc.custom_vpc.id
-  cidr_block              = "10.10.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-}
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
-resource "aws_subnet" "public_b" {
-  vpc_id                  = aws_vpc.custom_vpc.id
-  cidr_block              = "10.10.3.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-}
-
-resource "aws_subnet" "public_c" {
-  vpc_id                  = aws_vpc.custom_vpc.id
-  cidr_block              = "10.10.5.0/24"
-  availability_zone       = "us-east-1c"
-  map_public_ip_on_launch = true
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.custom_vpc.id
-}
-
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.custom_vpc.id
-}
-
-resource "aws_route" "default_route" {
-  route_table_id         = aws_route_table.public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
-resource "aws_route_table_association" "public_a_association" {
-  subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "public_b_association" {
-  subnet_id      = aws_subnet.public_b.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "public_c_association" {
-  subnet_id      = aws_subnet.public_c.id
-  route_table_id = aws_route_table.public_rt.id
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
 }
