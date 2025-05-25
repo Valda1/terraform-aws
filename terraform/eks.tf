66d563ceddba6a1fc30bd8710421a22f414b9daa
Valda <valda.bimbirule1@gmail.com>
2025-05-25 19:26:37 +0300

    EKS

---
+++ terraform/eks.tf
@@ -0,0 +1,14 @@
+module "eks" {
+    source = "terraform-aws-modules/eks/aws"
+    version = "~> 19.0"
+
+    cluster_name = "my-cluster-eks"
+    cluster_version = "1.27"
+
+    cluster_endpoint_public_access = true
+
+    vpc_id = module.vpc.vpc_id
+    subnet_ids = module.vpc.private_subnets #Worker nodes subnets
+    #subnet_ids = concat(module.vpc.public_subnets, module.vpc.private_subnets)
+    control_plane_subnet_ids = module.vpc.private_subnets
+}
