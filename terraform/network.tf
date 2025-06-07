034c75f583abadde9707be983a7bec779d7c53ae
Valda <valda.bimbirule1@gmail.com>
2025-06-07 14:33:37 +0300

    ALB

--- terraform/network.tf
+++ terraform/network.tf
@@ -10,3 +10,23 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_nat_gateway   = true
 }
+
+module "alb_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.1" # Check for latest version
+
+  name        = "alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  egress_rules  = ["all-all"]
+}
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+  name = "alb"
+  vpc_id = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+  security_groups = [aws_security_group.alb_sg.id]
+}
